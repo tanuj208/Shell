762b8228bb26cdb26c358bd2cda60689bdfb2262
Tanuj Garg <tanuj.garg@students.iiit.ac.in>
2018-09-27 23:41:30 +0530

    very minor change

--- overkill.c
+++ overkill.c
@@ -4,7 +4,12 @@ void shell_overkill(char **command, int processes[])
 {
     int i = 0;
     for(i = 0 ; i < 1000 ; i ++)
+    {
         if(processes[i] != -1)
+        {
             kill(processes[i], 9);
+            processes[i] = -1;
+        }
+    }
     return;
 }
